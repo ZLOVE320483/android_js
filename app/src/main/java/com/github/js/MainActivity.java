d31ef39d528469265c7d5eff9204cb20d2a70539
zlove <zlove.zhang@musical.ly>
2018-01-29 14:39:06 +0800

    Add Js Call Android Function:shouldOverrideUrlLoading

--- app/src/main/java/com/github/js/MainActivity.java
+++ app/src/main/java/com/github/js/MainActivity.java
@@ -1,9 +1,12 @@
 package com.github.js;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,14 +22,31 @@ public class MainActivity extends AppCompatActivity {
 
         // 设置与Js交互的权限
         webSettings.setJavaScriptEnabled(true);
+        // 设置允许JS弹窗
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
 
         // 通过addJavascriptInterface()将Java对象映射到JS对象
         //参数1：Javascript对象名
         //参数2：Java对象名
         webView.addJavascriptInterface(new AndroidToJs(), "jsCallback"); // AndroidToJs类对象映射到js的jsCallback对象
 
+        webView.setWebViewClient(webViewClient);
+
         // 加载JS代码
         // 格式规定为:file:///android_asset/文件名.html
         webView.loadUrl("file:///android_asset/js_call_android.html");
     }
+
+    private WebViewClient webViewClient = new WebViewClient() {
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            Uri uri = Uri.parse(url);
+            if (("scheme").equals(uri.getScheme())) {
+                Toast.makeText(MainActivity.this, url, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+            return super.shouldOverrideUrlLoading(view, url);
+        }
+    };
 }
